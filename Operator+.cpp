e0a52ec63ba51fbae6b5528f4a7b29924d06c921
yuzhu-noodles <qli@snnu.edu.cn>
2021-05-31 22:13:16 +0800

    第一次提交加号运算符重载代码

---
+++ Operator+.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+//加号运算符重载
+
+class Person{
+	public:
+		Person() {}
+		Person(int age){
+			m_Age = age;
+		}
+		//成员函数重载+
+/*  	Person operator+(const Person& p){
+			Person temp;   //这块必须自己设置默认构造函数，不然会出错
+			temp.m_Age = this->m_Age + p.m_Age;
+			return temp;
+		}
+		*/
+	public:
+		int m_Age;
+};
+
+//全局函数重载+
+Person operator+(const Person& p1,const Person& p2){
+	Person temp;
+	temp.m_Age = p1.m_Age + p2.m_Age;
+	return temp;
+}
+
+//本质：
+//Person p3 = p1.operator+(p2) //成员函数
+//Person p3 = operator+(p1,p2)  //全局函数
+
+//运算重载符也可以发生 函数重载
+Person operator+(const Person& p,int n){
+	Person temp;
+	temp.m_Age = p.m_Age + n;
+	return temp;
+}
+int main(){
+	Person p1(10);
+	Person p2(20);
+	Person p3 = p1 + p2;
+	cout<<"p3的年龄："<<p3.m_Age<<endl;
+	Person p4 = p1 + 100;
+	cout<<"p4的年龄："<<p4.m_Age<<endl;
+	return 0;
+}
