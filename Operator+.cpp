06f6142c4d42dd2b385a78fc101f07f71de023c8
yuzhu-noodles <qli@snnu.edu.cn>
2021-06-01 10:47:33 +0800

    第二提交Operator+.cpp,在代码中添加了其他双目运算符重载

--- Operator+.cpp
+++ Operator+.cpp
@@ -16,6 +16,30 @@ class Person{
 			return temp;
 		}
 		*/
+		//重载-
+		Person operator-(const Person& p){
+			Person temp;
+			temp.m_Age = this->m_Age - p.m_Age;
+			return temp;
+		}
+		//重载*
+        Person operator*(const Person& p){
+			Person temp;
+			temp.m_Age = this->m_Age * p.m_Age;
+			return temp;
+		}
+		//重载/
+        Person operator*(const Person& p){
+			Person temp;
+			temp.m_Age = this->m_Age / p.m_Age;
+			return temp;
+		//重载%
+        Person operator*(const Person& p){
+			Person temp;
+			temp.m_Age = this->m_Age / p.m_Age;
+			return temp;
+	public:
+
 	public:
 		int m_Age;
 };
@@ -37,12 +61,27 @@ Person operator+(const Person& p,int n){
 	temp.m_Age = p.m_Age + n;
 	return temp;
 }
-int main(){
+void test01(){  //测试operator+
 	Person p1(10);
 	Person p2(20);
 	Person p3 = p1 + p2;
 	cout<<"p3的年龄："<<p3.m_Age<<endl;
 	Person p4 = p1 + 100;
 	cout<<"p4的年龄："<<p4.m_Age<<endl;
+}
+void test02(){  //测试operator-
+	Person p1(20);
+	Person p2(10);
+	Person p3 = p1 - p2;
+	cout<<"p3的年龄："<<p3.m_Age<<endl;
+}
+void test03(){  //测试operator*
+    Person p1(10);
+	Person p2(10);
+	Person p3 = p1 * p2;
+	cout<<"p3的年龄："<<p3.m_Age<<endl;
+}
+int main(){
+	test03();
 	return 0;
 }
